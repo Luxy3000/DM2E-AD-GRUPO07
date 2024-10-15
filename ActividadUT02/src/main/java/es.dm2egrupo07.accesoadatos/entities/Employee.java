f0d273210af62428602da75bdc87773ab3d6f0d1
Luxy3000 <cilusu15@gmail.com>
2024-10-15 15:35:11 +0200

    Class Employee build

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/entities/Employee.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/entities/Employee.java
@@ -1,4 +1,19 @@
 package es.dm2egrupo07.accesoadatos.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+    private int employeeNumber;
+    private String lastName;
+    private String firstName;
+    private String extension;
+    private String email;
+    private String officeCode;
+    private int reportsTo;
+    private String jobTitle;
 }
