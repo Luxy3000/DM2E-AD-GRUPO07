e3174ee5e83c727e9c3e2c92177dc56ffc678c8c
Luxy3000 <cilusu15@gmail.com>
2024-10-15 16:52:31 +0200

    Employee interf and Impl
    Service level interface and implementation of Employee done

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/services/EmployeeService.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/services/EmployeeService.java
@@ -1,4 +1,25 @@
 package es.dm2egrupo07.accesoadatos.services;
 
+import es.dm2egrupo07.accesoadatos.entities.Employee;
+
+import java.util.List;
+
+//Interfaz para los métodos para trabajar con los empleados en la capa de servicios
 public interface EmployeeService {
+
+    //Buscar un empleado por su ID
+    Employee findById(int employeeNumber);
+
+    //Lista de todos los empleados
+    List<Employee> findAll();
+
+    //Actualizar indo de un empleado, true si funcionó
+    boolean updateEmployee(Employee employee);
+
+    //Borrar un empleado, true si funcinó
+    boolean deleteEmployee(int employeeNumber);
+
+    //Agregar un empleado, true si funcionó
+    boolean addEmployee(Employee employee);
+
 }
--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/services/EmployeeServiceImpl.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/services/EmployeeServiceImpl.java
@@ -1,4 +1,43 @@
 package es.dm2egrupo07.accesoadatos.services;
 
-public class EmployeeServiceImpl {
+import es.dm2egrupo07.accesoadatos.dataaccess.EmployeeDataAccess;
+import es.dm2egrupo07.accesoadatos.entities.Employee;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeDataAccess employeeDataAccess;
+
+    //Constructor que recibe una abstracción de EmployeeDataAccess
+    public EmployeeServiceImpl(EmployeeDataAccess employeeDataAccess) {
+        this.employeeDataAccess = employeeDataAccess;
+    }
+
+    @Override
+    public Employee findById(int employeeNumber) {
+        //Se delega la búsqueda a la parte de acceso a datos
+        return employeeDataAccess.findById(employeeNumber);
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        //Se delega la búsqueda a la parte de acceso a datos
+        return employeeDataAccess.findAll();
+    }
+
+    @Override
+    public boolean updateEmployee(Employee employee) {
+        return employeeDataAccess.update(employee);
+    }
+
+    @Override
+    public boolean deleteEmployee(int employeeNumber) {
+        return employeeDataAccess.delete(employeeNumber);
+    }
+
+    @Override
+    public boolean addEmployee(Employee employee) {
+        return employeeDataAccess.save(employee);
+    }
 }
