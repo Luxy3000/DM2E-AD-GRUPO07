e0505f2417b64b0f29872be35bbd67a43a93c782
AdriiLein <158595430+AdriiLein@users.noreply.github.com>
2024-10-16 16:51:14 +0200

    161024-16:51

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/ProductDataAccess.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/ProductDataAccess.java
@@ -2,9 +2,25 @@ package es.dm2egrupo07.accesoadatos.dataaccess;
 
 import es.dm2egrupo07.accesoadatos.entities.Product;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductDataAccess {
+
+    long count();
+    boolean existsById(String id);
+    Optional<Product> findById(String id);
     List<Product> findAll();
-    List<Product> findByCode(String code);
-    List<Product> findByName(String name);
+    Product save(Product product);
+    void deleteById(String id);
+
+
+    /*
+    • Constructor en el que reciba las dependencias necesarias, si es que las hay. Las dependencias, siempre que sea posible, se inyectarán como interfaces.
+    • long count() – Cuenta el número total de entidades en la tabla.
+    • boolean existsById(ID id) – Verifica si una entidad con un ID dado existe.
+    • Optional<T> findById(ID id) – Busca una entidad por su ID. Si no existe devuelve Optional vacío.
+    • List<T> findAll() – Recupera todas las entidades.
+    • T save(T entity) – Guarda una entidad. Si la entidad ya existe, la actualiza. Recibe la entidad que se va a guardar. Devuelve la entidad guardada, con los datos actualizados (id si es nueva)
+    • void deleteById(ID id) – Elimina la entidad con el ID especificado.
+    * */
 }
--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/ProductDataAccessImpl.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/ProductDataAccessImpl.java
@@ -2,22 +2,153 @@ package es.dm2egrupo07.accesoadatos.dataaccess;
 
 import es.dm2egrupo07.accesoadatos.entities.Product;
 
-import java.util.List;
+import java.sql.*;
+import java.util.*;
 
 public class ProductDataAccessImpl implements ProductDataAccess {
 
     @Override
+    public long count() {
+        try (Connection connection = ConnectionPool.getInstance().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("select count(*) from products");
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            if (resultSet.next()) {
+                return resultSet.getLong(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al contar productos", e);
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean existsById(String id) {
+        try (Connection connection = ConnectionPool.getInstance().getConnection();
+             PreparedStatement ps = connection.prepareStatement("select count(*) from products where productCode = ?")) {
+            ps.setString(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    int count = rs.getInt(1);
+                    return count > 0;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al verificar existencia de producto", e);
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<Product> findById(String id) {
+        try (Connection connection = ConnectionPool.getInstance().getConnection();
+            PreparedStatement ps = connection.prepareStatement("select * from products where productCode = ?")) {
+            ps.setString(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return Optional.of(mapRsProduct(rs));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al buscar productos", e);
+        }
+        return Optional.empty();
+    }
+
+    @Override
     public List<Product> findAll() {
-        return List.of();
+        List<Product> products = new ArrayList<>();
+        try (Connection connection = ConnectionPool.getInstance().getConnection();
+            PreparedStatement ps = connection.prepareStatement("select * from products")) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        products.add(mapRsProduct(rs));
+                    }
+                }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al buscar productos", e);
+        }
+        return products;
     }
 
     @Override
-    public List<Product> findByCode(String code) {
-        return List.of();
+    public Product save(Product product) {
+        if (existsById(product.getProductCode())){
+
+        }
+        return null;
     }
 
     @Override
-    public List<Product> findByName(String name) {
-        return List.of();
+    public void deleteById(String id) {
+        try(Connection connection = ConnectionPool.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("delete from products where productcode = ?")){
+            preparedStatement.setString(1, id);
+            preparedStatement.executeUpdate();
+            System.out.println("El producto con id: " + id + "se ha eliminado correctamente");
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al buscar productos", e);
+        }
     }
+
+    private boolean create(Product product) {
+
+        String sqlSave = "INSERT INTO products (productCode, productName, productLine, productScale, productVendor, productDescription, quantityInStock, buyPrice, MSRP) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try(Connection connection = ConnectionPool.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sqlSave)){
+            preparedStatement.setString(1, product.getProductCode());
+            preparedStatement.setString(2, product.getProductName());
+            preparedStatement.setString(3, product.getProductLine());
+            preparedStatement.setString(4, product.getProductScale());
+            preparedStatement.setString(5, product.getProductVendor());
+            preparedStatement.setString(6, product.getProductDescription());
+            preparedStatement.setInt(7, product.getQuantityInStock());
+            preparedStatement.setDouble(8, product.getBuyPrice());
+            preparedStatement.setDouble(9, product.getMsrp());
+
+            return preparedStatement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al buscar productos", e);
+        }
+    }
+
+    private boolean update(Product product) {
+        String sqlUpdate =  "UPDATE products SET productName=?, productLine=?, productScale=?, productVendor=?, productDescription=?, quantityInStock=?, buyPrice=?, MSRP=? WHERE productCode=?";
+
+        try(Connection connection = ConnectionPool.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlUpdate)){
+            preparedStatement.setString(1, product.getProductName());
+            preparedStatement.setString(2, product.getProductLine());
+            preparedStatement.setString(3, product.getProductScale());
+            preparedStatement.setString(4, product.getProductVendor());
+            preparedStatement.setString(5, product.getProductDescription());
+            preparedStatement.setInt(6, product.getQuantityInStock());
+            preparedStatement.setDouble(7, product.getBuyPrice());
+            preparedStatement.setDouble(8, product.getMsrp());
+            preparedStatement.setString(9, product.getProductCode());
+
+            return preparedStatement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al buscar productos", e);
+        }
+    }
+
+
+    private Product mapRsProduct(ResultSet rs) throws SQLException {
+        return new Product(
+            rs.getString("productCode"),
+            rs.getString("productName"),
+            rs.getString("productLine"),
+            rs.getString("productScale"),
+            rs.getString("productVendor"),
+            rs.getString("productDescription"),
+            rs.getInt("quantityInStock"),
+            rs.getDouble("buyPrice"),
+            rs.getDouble("MSRP")
+        );
+
+    }
+
 }
