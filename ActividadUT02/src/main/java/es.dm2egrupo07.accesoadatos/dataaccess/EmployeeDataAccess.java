860af05d6a4ef7360bd7fd7f999dd4e2df946ab1
Luxy3000 <cilusu15@gmail.com>
2024-10-16 16:12:42 +0200

    Cambio para ajuste del proyecto
    Se han cambiado las acciones para que sean las que pide el documento del proyecto, siendo del tipo y devolviendo la respuesta que piden.

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/EmployeeDataAccess.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/EmployeeDataAccess.java
@@ -2,12 +2,16 @@ package es.dm2egrupo07.accesoadatos.dataaccess;
 
 import es.dm2egrupo07.accesoadatos.entities.Employee;
 import java.util.List;
+import java.util.Optional;
+
 //Definir las acciones que se pueden realizar sobre la entidad Employee
 //Es una plantilla predefinida
 public interface EmployeeDataAccess {
+
     //Encontrar empleado por el número de empleado,
     //usa numero del empleado como parametro y devuelve obejeto.
-    Employee findById(int employeeNumber);
+    //Employee findById(int employeeNumber); --> Eliminado porque puede dar NullPointerException si no llega a encontrar al employee
+    Optional<Employee> findEmployee(int employeeNumber);
 
     //Obtener lista de todos los empreados
     //devuelve lista de objetos Employee
@@ -15,13 +19,20 @@ public interface EmployeeDataAccess {
 
     //Acción 1, guardar nuevo empleado en la BBDD
     //Recibe objeto Employee y devuelve true si funciona
-    boolean save(Employee employee);
+    Employee save(Employee employee);
 
     //Acción 2, actualiar info de un empleado existente
     //Recibe objeto employee y devuelve true si funciona
-    boolean update(Employee employee);
+    Employee update(Employee employee);
 
     //Acción 3, eliminar un empleado de la BBDD
     //Recibe numero de empleado y devuelve true si funciona
-    boolean delete(int employeeNumber);
+    void deleteById(int employeeNumber);
+
+    //Accion 4, dar la longitud de la tabla de empleados
+    long count();
+
+    //Accion 5, verificar si existe un empleado con un id
+    boolean existById(int employeeNumber);
+
 }
--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/EmployeeDataAccessImpl.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/EmployeeDataAccessImpl.java
@@ -1,6 +1,5 @@
 package es.dm2egrupo07.accesoadatos.dataaccess;
 
-import com.zaxxer.hikari.HikariDataSource;
 import es.dm2egrupo07.accesoadatos.entities.Employee;
 
 import java.sql.Connection;
@@ -9,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 //La implementacion de la interfaz da el cómo funcionan las acciones establecidas en la interfaz
 //Indica cómo se hacen esas acciones definidas en la interfaz.
@@ -17,32 +17,32 @@ public class EmployeeDataAccessImpl implements EmployeeDataAccess {
     private final ConnectionPool connectionPool;
 
     //Constructor que recibe el pool, permite reuso para mejor rendimiento
-    public EmployeeDataAccessImpl(HikariDataSource dataSource) {
+    public EmployeeDataAccessImpl() {
         this.connectionPool = ConnectionPool.getInstance();
     }
 
     @Override
-    public Employee findById(int employeeNumber) {
+    public Optional<Employee> findEmployee(int employeeNumber) {
         //Predefinimos la consulta
         String sql = "SELECT * FROM employee WHERE employee_number = ?";
 
         //Try with resources para hacer una conexión segura
         try (Connection conn = connectionPool.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql)) {
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
             //Asignar el valor del parámetro en la consulta
             stmt.setInt(1, employeeNumber);
 
             //Ejecutar la consulta y obtener resultado
-            ResultSet rs = stmt.executeQuery();
-
-            //Si hay result, lo hacemos objeto Employee
-            if (rs.next()) {
-                return mapToEmployee(rs);
+            try(ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    //Si hay result, lo hacemos objeto Employee
+                    return Optional.of(mapToEmployee(rs));
+                }
             }
         }catch ( SQLException e ) {
             e.printStackTrace();
         }
-        return null;
+        return Optional.empty();
     }
 
 
@@ -54,8 +54,8 @@ public class EmployeeDataAccessImpl implements EmployeeDataAccess {
 
         //Ejecutamos la consulta
         try(Connection conn = connectionPool.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        ResultSet rs = stmt.executeQuery()) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery()) {
             //Iteramos sobre el resultado mapeando cada fila
             while (rs.next()) {
                 employees.add(mapToEmployee(rs));
@@ -68,12 +68,19 @@ public class EmployeeDataAccessImpl implements EmployeeDataAccess {
     }
 
     @Override
-    public boolean save(Employee employee) {
+    public Employee save(Employee employee) {
+        //Verificar que el empleado no existe ya en la BBSS
+        if(existById(employee.getEmployeeNumber())) {
+            //Si ya existe se manda a update para guardar si hay algún cambio en el employees
+            update(employee);
+            return employee;
+        }
+
         //Definir la consulta SQL para insertar nuevo empleado
         String sql = "INSERT INTO employees (employee_number,lastName, firstName, extension, email, officeCode, reportsTo, jobTitle) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
         try(Connection conn = connectionPool.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql)){
+            PreparedStatement stmt = conn.prepareStatement(sql)){
             //Mapeamos todos los valores
             stmt.setInt(1, employee.getEmployeeNumber());
             stmt.setString(2, employee.getLastName());
@@ -85,22 +92,23 @@ public class EmployeeDataAccessImpl implements EmployeeDataAccess {
             stmt.setString(8, employee.getJobTitle());
 
             //Si es exitoso el valor de stmt.executeUpdate() será un número mayor a 0 por lo que el return dará un true.
-            return stmt.executeUpdate() > 0;
+            stmt.executeUpdate();
+            return employee;
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
         //Si hay algún error devuelve false
-        return false;
+        return null;
     }
 
     @Override
-    public boolean update(Employee employee) {
+    public Employee update(Employee employee) {
         //Definir la consulta SQL para actualizar
         String sql =  "UPDATE employees SET lastName = ?,firstName = ?,extension = ?,email = ?,officeCode = ?, reportsTo = ?, jobTitle = ? WHERE employee_number = ?";
 
         try(Connection conn = connectionPool.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql)) {
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             //Asignamos los valores
             stmt.setString(1, employee.getLastName());
@@ -113,26 +121,57 @@ public class EmployeeDataAccessImpl implements EmployeeDataAccess {
             stmt.setInt(8, employee.getEmployeeNumber());
 
             //Devuelve true si funciona
-            return stmt.executeUpdate() > 0;
+            stmt.executeUpdate();
+            return employee;
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return false;
+        return null;
     }
 
     @Override
-    public boolean delete(int employeeNumber) {
+    public void deleteById(int employeeNumber) {
         //Preparamos la consulta SQL para eliminar un employee
         String sql = "DELETE FROM employees WHERE employee_number = ?";
 
         try(Connection conn = connectionPool.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql)) {
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
             //Damos valores a la consulta
             stmt.setInt(1, employeeNumber);
 
             //Devulvemos true si funciona
-            return stmt.executeUpdate() > 0;
+            stmt.executeUpdate();
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public long count() {
+        String sql = "SELECT COUNT(*) FROM employees";
+        try (Connection conn = connectionPool.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            if(rs.next()) {
+                return rs.getLong(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean existById(int employeeNumber) {
+        String sql = "SELECT * FROM employees WHERE employee_number = ?";
+        try(Connection connection = connectionPool.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setInt(1, employeeNumber);
+            try(ResultSet rs = stmt.executeQuery()) {
+                return rs.next();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
