2c162104069a851176e0b1fc831562d1cd2757a1
VeraSDC <146889962+VeraSDC@users.noreply.github.com>
2024-10-18 13:35:30 +0200

    Entity Customer

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/entities/Customer.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/entities/Customer.java
@@ -1,5 +1,27 @@
 package es.dm2egrupo07.accesoadatos.entities;
 
-public class Customer {
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Customer {
+  @EqualsAndHashCode.Include
+    private int customerNumber;
+    private String customerName;
+    private String contactLastName;
+    private String contactFirstName;
+    private String phone;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private int salesRepEmployeeNumber;
+    private double creditLimit;
 }
