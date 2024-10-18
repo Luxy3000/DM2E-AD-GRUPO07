d8bc25b2de3933acb76172def200297464f7ba5a
VeraSDC <146889962+VeraSDC@users.noreply.github.com>
2024-10-18 13:37:27 +0200

    creado CustomerDataAccess

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/CustomerDataAccess.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/CustomerDataAccess.java
@@ -1,4 +1,15 @@
 package es.dm2egrupo07.accesoadatos.dataaccess;
 
+import es.dm2egrupo07.accesoadatos.entities.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerDataAccess {
+  long count();
+    boolean existsById(int id);
+    Optional<Customer> findById(int id);
+    List<Customer> findAll();
+    Customer save(Customer customer);
+    void deleteById(int id);
 }
