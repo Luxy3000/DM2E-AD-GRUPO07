1bdc933f201e108db911040200d3b1a999beeaee
VeraSDC <146889962+VeraSDC@users.noreply.github.com>
2024-10-22 09:43:10 +0200

    Update CustomerService

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/services/CustomerService.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/services/CustomerService.java
@@ -1,4 +1,15 @@
 package es.dm2egrupo07.accesoadatos.services;
 
+import es.dm2egrupo07.accesoadatos.entities.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerService {
+  Long count();
+    Boolean existsById(int id);
+    Optional<Customer> findById(int id);
+    List<Customer> findAll();
+    Customer save(Customer customer);
+    void deleteById(int id);
 }
