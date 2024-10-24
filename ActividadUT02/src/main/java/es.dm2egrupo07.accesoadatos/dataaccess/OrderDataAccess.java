3078191fedbe51b1de9752b792df15dd3b1f1260
VeraSDC <146889962+VeraSDC@users.noreply.github.com>
2024-10-24 12:35:18 +0200

    Update + Documentation OrderDataAccess.java

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/OrderDataAccess.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/OrderDataAccess.java
@@ -1,4 +1,22 @@
 package es.dm2egrupo07.accesoadatos.dataaccess;
 
+import es.dm2egrupo07.accesoadatos.entities.Order;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Interfaz para el acceso a datos de pedidos.
+ * Tiene métodos para realizar operaciones sobre los pedidos como
+ * buscar por id del pedido, dar una lista de todos los pedidos,
+ * agregar, actualizar, borrar, contar o comprobar si existe.
+ * */
 public interface OrderDataAccess {
+    long count();
+    boolean existsById(int id);
+    Optional<Order> findById(int id);
+    List<Order> findAll();
+    Order save(Order order);
+    Order update(Order order);
+    void deleteById(int id);
 }
