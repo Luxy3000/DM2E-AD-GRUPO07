fc7bef815eed1823d4a298bb4d72bf028d120681
VeraSDC <146889962+VeraSDC@users.noreply.github.com>
2024-10-24 19:00:15 +0200

    Update + Documentation CustomerService.java

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/services/CustomerService.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/services/CustomerService.java
@@ -5,11 +5,17 @@ import es.dm2egrupo07.accesoadatos.entities.Customer;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Interfaz que define los servicios de los clientes.
+ * Tiene métodos para realizar operaciones básicas como buscar, guardar,
+ * actualizar, borrar, contar o comprobar si existe un cliente.
+ * */
 public interface CustomerService {
-  Long count();
-    Boolean existsById(int id);
+    long count();
+    boolean existsById(int id);
     Optional<Customer> findById(int id);
     List<Customer> findAll();
     Customer save(Customer customer);
+    Customer update(Customer customer);
     void deleteById(int id);
 }
