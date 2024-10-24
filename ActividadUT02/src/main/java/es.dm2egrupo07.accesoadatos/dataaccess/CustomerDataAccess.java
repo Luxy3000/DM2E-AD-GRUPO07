86c34407eba9a9959c9330a30da47c35d599e6f6
VeraSDC <146889962+VeraSDC@users.noreply.github.com>
2024-10-24 12:34:33 +0200

    Update + Documentation CustomerDataAccess.java

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/CustomerDataAccess.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/CustomerDataAccess.java
@@ -5,11 +5,18 @@ import es.dm2egrupo07.accesoadatos.entities.Customer;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Interfaz para el acceso a datos de clientes.
+ * Tiene métodos para realizar operaciones sobre los clientes como
+ * buscar por id del cliente, dar una lista de todos los clientes,
+ * agregar, actualizar, borrar, contar o comprobar si existe.
+ * */
 public interface CustomerDataAccess {
-  long count();
+    long count();
     boolean existsById(int id);
     Optional<Customer> findById(int id);
     List<Customer> findAll();
     Customer save(Customer customer);
+    Customer update(Customer customer);
     void deleteById(int id);
 }
