28f70f8aea74bab850e58d21ecf06dc36e0cbb5f
VeraSDC <146889962+VeraSDC@users.noreply.github.com>
2024-10-26 21:57:29 +0200

    Update + Documentation OrderService.java

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/services/OrderService.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/services/OrderService.java
@@ -1,4 +1,21 @@
 package es.dm2egrupo07.accesoadatos.services;
 
+import es.dm2egrupo07.accesoadatos.entities.Order;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Interfaz que define los servicios de los pedidos.
+ * Tiene métodos para realizar operaciones básicas como buscar, guardar,
+ * actualizar, borrar, contar o comprobar si existe un pedido.
+ * */
 public interface OrderService {
+    long count();
+    boolean existsById(int id);
+    Optional<Order> findById(int id);
+    List<Order> findAll();
+    Order save(Order order);
+    Order update(Order order);
+    void deleteById(int id);
 }
