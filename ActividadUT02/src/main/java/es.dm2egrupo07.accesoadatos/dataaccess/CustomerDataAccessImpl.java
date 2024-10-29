b43adcbfe129e4f2514381776dce8ef32b0e7d27
AdriiLein <158595430+AdriiLein@users.noreply.github.com>
2024-10-29 16:19:22 +0100

    Add customer import

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/CustomerDataAccessImpl.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/dataaccess/CustomerDataAccessImpl.java
@@ -1,5 +1,7 @@
 package es.dm2egrupo07.accesoadatos.dataaccess;
 
+import es.dm2egrupo07.accesoadatos.entities.Customer;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
