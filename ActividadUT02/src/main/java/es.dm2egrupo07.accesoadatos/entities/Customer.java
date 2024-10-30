05d06ddadc63acc4387cda0f6d8a59880b7bdc72
VeraSDC <146889962+VeraSDC@users.noreply.github.com>
2024-10-30 01:28:04 +0100

    Documentation Customer.java

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/entities/Customer.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/entities/Customer.java
@@ -14,18 +14,69 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Customer {
+    /**
+     * El número único del cliente.
+     */
     @EqualsAndHashCode.Include
     private int customerNumber;
+
+    /**
+     * El nombre completo del cliente.
+     */
     private String customerName;
+
+    /**
+     * El apellido del cliente.
+     */
     private String contactLastName;
+
+    /**
+     * El nombre del cliente.
+     */
     private String contactFirstName;
+
+    /**
+     * El número de teléfono del cliente.
+     */
     private String phone;
+
+    /**
+     * La primera línea de la dirección del cliente.
+     */
     private String addressLine1;
+
+    /**
+     * La segunda línea de la dirección del cliente, la mayoría vacías.
+     */
     private String addressLine2;
+
+    /**
+     * El nombre de la ciudad donde vive el cliente.
+     */
     private String city;
+
+    /**
+     * El nombre del estado donde vive el cliente.
+     */
     private String state;
+
+    /**
+     * El código postal donde vive el cliente.
+     */
     private String postalCode;
+
+    /**
+     * El nombre del país donde vive el cliente.
+     */
     private String country;
+
+    /**
+     * El número de ventas que ha realizado el empleado.
+     */
     private int salesRepEmployeeNumber;
+
+    /**
+     * La cantidad de límite de crédito que tiene el cliente.
+     */
     private double creditLimit;
 }
