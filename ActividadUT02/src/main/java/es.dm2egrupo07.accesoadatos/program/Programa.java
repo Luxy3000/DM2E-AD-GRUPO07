fe2941f094b20e4e79a4c3b9cec77c0f6e65cc25
Luxy3000 <cilusu15@gmail.com>
2024-11-03 14:53:20 +0100

    Tests testing

--- ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/program/Programa.java
+++ ActividadUT02/src/main/java/es.dm2egrupo07.accesoadatos/program/Programa.java
@@ -1,8 +1,10 @@
 package es.dm2egrupo07.accesoadatos.program;
 
+import es.dm2egrupo07.accesoadatos.application.CustomerTests;
 import es.dm2egrupo07.accesoadatos.application.EmployeeTests;
 import es.dm2egrupo07.accesoadatos.application.OrderTests;
 import es.dm2egrupo07.accesoadatos.dataaccess.OrderDataAccessImpl;
+import es.dm2egrupo07.accesoadatos.services.CustomerService;
 import es.dm2egrupo07.accesoadatos.services.EmployeeService;
 import es.dm2egrupo07.accesoadatos.services.OrderService;
 import es.dm2egrupo07.accesoadatos.services.OrderServiceImpl;
@@ -10,7 +12,9 @@ import es.dm2egrupo07.accesoadatos.services.OrderServiceImpl;
 public class Programa {
 
     public static void main(String[] args) {
-        new EmployeeTests();
 
+        //new EmployeeTests();
+        //new OrderTests();
+        new CustomerTests();
     }
 }
