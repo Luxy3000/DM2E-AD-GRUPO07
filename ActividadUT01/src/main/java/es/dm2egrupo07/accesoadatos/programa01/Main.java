4227a7bba6ce6e810f067bc289dd974dbed4248f
AdriiLein <158595430+AdriiLein@users.noreply.github.com>
2024-11-05 16:37:16 +0100

    Clase WeatherDataWriter

--- ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/Main.java
+++ ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/Main.java
@@ -1,7 +1,22 @@
 package es.dm2egrupo07.accesoadatos.programa01;
 
+
+import java.util.Scanner;
+
 public class Main {
+
+    private static final WeatherDataWriter weatherDataWriter = new WeatherDataWriter();
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args){
 
+
+        System.out.println("¿Donde te gustaría guardar el fichero XML?");
+        String outputFile = scanner.nextLine();
+        if (weatherDataWriter.ubicacionXmlSalida(outputFile)) {
+            System.out.println("Generando el fichero...");
+        } else {
+            System.out.println("El programa se ha terminado.");
+        }
     }
 }
