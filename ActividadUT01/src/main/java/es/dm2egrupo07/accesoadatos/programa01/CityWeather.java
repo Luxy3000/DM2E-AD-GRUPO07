10588bfeb787e465d3fc5e55206fda69a535caf0
Luxy3000 <cilusu15@gmail.com>
2024-11-05 16:49:56 +0100

    CityWeather iniciated
    Agregada la parte de la clase para manejar la información de las ciudades para manejar las estadísticas

--- ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/CityWeather.java
+++ ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/CityWeather.java
@@ -1,7 +1,35 @@
 package es.dm2egrupo07.accesoadatos.programa01;
 
+import lombok.Getter;
+import java.util.List;
+
 /**
  * Clase creada para manejar los datos de una ciudad
  */
+@Getter
 public class CityWeather {
+    /* Seguir editando según se necesite para las otras clases */
+    private String name;
+    private List<Double> temperatures;
+    private List<Double> humidities;
+    private List<Double> pressures;
+
+    public CityWeather(String name) {
+        this.name = name;
+        this.temperatures = new ArrayList<>();
+        this.humidities = new ArrayList<>();
+        this.pressures = new ArrayList<>();
+    }
+
+    public void addTemperature(double temp) {
+        temperatures.add(temp);
+    }
+
+    public void addHumidity(double hum) {
+        humidities.add(hum);
+    }
+
+    public void addPressure(double p) {
+        pressures.add(p);
+    }
 }
