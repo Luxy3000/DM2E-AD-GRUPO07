3c930583ec60fd1aabb722533072617de177a7da
AdriiLein <158595430+AdriiLein@users.noreply.github.com>
2024-11-07 17:03:30 +0100

    Update WeatherDataWriter Final

--- ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/Main.java
+++ ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/Main.java
@@ -1,6 +1,8 @@
 package es.dm2egrupo07.accesoadatos.programa01;
 
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,15 +10,18 @@ public class Main {
     private static final WeatherDataWriter weatherDataWriter = new WeatherDataWriter();
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException {
 
 
-        System.out.println("¿Donde te gustaría guardar el fichero XML?");
+       /* System.out.println("¿Donde te gustaría guardar el fichero XML?");
         String outputFile = scanner.nextLine();
         if (weatherDataWriter.ubicacionXmlSalida(outputFile)) {
             System.out.println("Generando el fichero...");
         } else {
             System.out.println("El programa se ha terminado.");
-        }
+        }*/
+
+        WeatherDataWriter weatherDataWriter = new WeatherDataWriter();
+        weatherDataWriter.generarXml();
     }
 }
--- ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/WeatherDataWriter.java
+++ ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/WeatherDataWriter.java
@@ -4,6 +4,8 @@ import org.w3c.dom.*;
 
 import javax.xml.parsers.*;
 import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.util.*;
 
@@ -12,10 +14,11 @@ import java.util.*;
  */
 public class WeatherDataWriter {
 
-    public void generarXml() throws ParserConfigurationException {
+    public static final int INDENT = 5;
+
+    public void generarXml() throws ParserConfigurationException, TransformerException {
 
         List<CityWeather> cities = getCityWeatherData();
-        List<CityStatistics> statisticsList = WeatherStatistics.calculateStatistics(cities);
 
         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         Document xmlDocument = builder.newDocument();
@@ -23,17 +26,49 @@ public class WeatherDataWriter {
         xmlDocument.setXmlStandalone(true);
 
         Element citiesElement = xmlDocument.createElement("city-weather-summary");
+        xmlDocument.appendChild(citiesElement);
 
         for (CityWeather cityWeather : cities) {
-            Element cityNameElement = xmlDocument.createElement("city");
-            cityNameElement.setAttribute("id", cityWeather.getId());
-            cityNameElement.setAttribute("name", cityWeather.getName());
 
-            Element cityAvgTemp = xmlDocument.createElement("average-temperature");
+            double minTemperature = WeatherStatistics.calculateMin(cityWeather.getTemperatures());
+            double maxTemperature = WeatherStatistics.calculateMax(cityWeather.getTemperatures());
+            double avgTemperature = WeatherStatistics.calculateAverage(cityWeather.getTemperatures());
+            double avgHumidity = WeatherStatistics.calculateAverage(cityWeather.getHumidities());
+            double avgPressure = WeatherStatistics.calculateAverage(cityWeather.getPressures());
+
+            Element cityElement = xmlDocument.createElement("city");
+            cityElement.setAttribute("id", cityWeather.getId());
+            cityElement.setAttribute("name", cityWeather.getName());
+
+            Element avgTempElement = xmlDocument.createElement("average-temperature");
+
             Element avgElement = xmlDocument.createElement("avg");
+            avgElement.appendChild(xmlDocument.createTextNode(String.format("%.1f", avgTemperature)));
 
-            citiesElement.appendChild(cityNameElement);
+            Element maxElement = xmlDocument.createElement("max");
+            maxElement.appendChild(xmlDocument.createTextNode(String.format("%.1f", maxTemperature)));
+
+            Element minElement = xmlDocument.createElement("min");
+            minElement.appendChild(xmlDocument.createTextNode(String.format("%.1f", minTemperature)));
+
+            avgTempElement.appendChild(avgElement);
+            avgTempElement.appendChild(maxElement);
+            avgTempElement.appendChild(minElement);
+
+            Element humidityElement = xmlDocument.createElement("average-humidity");
+            humidityElement.appendChild(xmlDocument.createTextNode(String.format("%.1f", avgHumidity)));
+
+            Element pressureElement = xmlDocument.createElement("average-pressure");
+            pressureElement.appendChild(xmlDocument.createTextNode(String.format("%.1f", avgPressure)));
+
+            cityElement.appendChild(avgTempElement);
+            cityElement.appendChild(humidityElement);
+            cityElement.appendChild(pressureElement);
+
+            citiesElement.appendChild(cityElement);
         }
+
+        dumpToConsole(xmlDocument);
 
     }
 
@@ -74,5 +109,17 @@ public class WeatherDataWriter {
         } else {
             return true;
         }
+    }
+
+
+    private static void dumpToConsole(Document newDoc) throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        transformerFactory.setAttribute("indent-number", INDENT);
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        Source src = new DOMSource(newDoc);
+        Result dest = new StreamResult(System.out);
+        transformer.transform(src, dest);
     }
 }
