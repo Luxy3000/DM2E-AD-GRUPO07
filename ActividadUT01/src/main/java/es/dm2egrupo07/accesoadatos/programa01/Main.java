8d8e0ff7a2847294a8d1d5358007c16d1484a2dd
VeraSDC <146889962+VeraSDC@users.noreply.github.com>
2024-11-09 00:12:43 +0100

    Update Main.java programa01

--- ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/Main.java
+++ ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/Main.java
@@ -7,11 +7,14 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static final WeatherDataReader weatherDataReader = new WeatherDataReader();
     private static final WeatherDataWriter weatherDataWriter = new WeatherDataWriter();
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws ParserConfigurationException, TransformerException {
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException, IOException, SAXException {
 
+        Path ubicacion = weatherDataReader.preguntarUbicacionXML();
+        weatherDataReader.leerFichero(ubicacion);
 
        System.out.println("¿Donde te gustaría guardar el fichero XML?");
         String outputFile = scanner.nextLine();
