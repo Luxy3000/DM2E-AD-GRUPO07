f5561a81af8e715d3e3e81ff574aa959b10cd759
VeraSDC <146889962+VeraSDC@users.noreply.github.com>
2024-11-09 00:10:37 +0100

    Documentation WeatherDataReader.java programa01

--- ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/WeatherDataReader.java
+++ ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa01/WeatherDataReader.java
@@ -17,7 +17,11 @@ import java.util.Scanner;
  */
 public class WeatherDataReader {
   private static final Scanner scanner = new Scanner(System.in);
-  
+
+  /**
+     * Método para preguntar al usuario la ruta del fichero a leer y si el fichero existe.
+     * @return el camino al archivo
+     */
     public Path preguntarUbicacionXML() {
         System.out.println("Introduzca la ruta del fichero que quiera leer: ");
         String ruta = scanner.nextLine();
@@ -33,16 +37,28 @@ public class WeatherDataReader {
         }
     }
 
+  /**
+     * Método para leer el fichero que se encuentre en la ubicación recogida en el método anterior.
+     * @param ubicacion
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     */
     public void leerFichero(Path ubicacion) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         Document fichero = builder.parse(ubicacion.toFile());
         fichero.getDocumentElement().normalize();
 
+        /**
+         * Para comprobar que se puede recoger infomación del fichero, lo que a su vez nos demuestra que se ha leido
+         * corrctamente.
+         */
+        /*
         NodeList nodos = fichero.getElementsByTagName("city");
         int numeroElementos = nodos.getLength();
         for (int i = 0; i < numeroElementos; i++) {
             Node nodo = nodos.item(i);
             System.out.println(nodo.getTextContent());
-        }
+        }*/
     }
 }
