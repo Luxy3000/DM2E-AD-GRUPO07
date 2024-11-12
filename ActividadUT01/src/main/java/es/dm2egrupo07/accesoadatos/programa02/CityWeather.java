3c7e303d7979b6165112d653ea969f6a7c026d7e
Luxy3000 <cilusu15@gmail.com>
2024-11-12 16:38:53 +0100

    CityWeather elaborated

--- ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa02/CityWeather.java
+++ ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa02/CityWeather.java
@@ -1,7 +1,43 @@
 package es.dm2egrupo07.accesoadatos.programa02;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.*;
+
 /**
  * Clase creada para manejar los datos de una ciudad
  */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CityWeather {
+    private String name;
+    private String id;
+    private List<Double> temperatures;
+    private List<Double> humidities;
+    private List<Double> pressures;
+
+    public CityWeather(String id, String name){
+        this.id = id;
+        this.name = name;
+        this.temperatures = new ArrayList<>();
+        this.humidities = new ArrayList<>();
+        this.pressures = new ArrayList<>();
+    }
+
+    private void addTemperature(double temp) {
+        temperatures.add(temp);
+    }
+
+    public void addHumidity(double hum) {
+        humidities.add(hum);
+    }
+
+    public void addPressure(double p) {
+        pressures.add(p);
+    }
 }
