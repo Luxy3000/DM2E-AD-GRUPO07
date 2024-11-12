b67c5a0274c797e8926e4803de2939ac507a9f51
Luxy3000 <cilusu15@gmail.com>
2024-11-12 16:39:10 +0100

    Create CityStatistics class

---
+++ ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa02/CityStatistics.java
@@ -0,0 +1,33 @@
+package es.dm2egrupo07.accesoadatos.programa02;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Clase creada para almacenar los resultados estadísticos de cada ciudad.
+ * Contiene los valores promedio, máximo y mínimo de temperatura,
+ * así como la humedad y presión promedio.
+ */
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CityStatistics {
+    /** Nombre de la ciudad. */
+    private String cityName;
+
+    /** Temperatura promedio de la ciudad. */
+    private double avgTemperature;
+
+    /** Temperatura mínima registrada en la ciudad. */
+    private double minTemperature;
+
+    /** Temperatura máxima registrada en la ciudad. */
+    private double maxTemperature;
+
+    /** Humedad promedio de la ciudad. */
+    private double avgHumidity;
+
+    /** Presión atmosférica promedio de la ciudad. */
+    private double avgPressure;
+}
