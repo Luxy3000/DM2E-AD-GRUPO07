39d341fe511152bf5c70f460027e8b841b022403
Luxy3000 <cilusu15@gmail.com>
2024-11-12 16:39:36 +0100

    Statistics handles class elaborated

--- ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa02/WeatherStatistics.java
+++ ActividadUT01/src/main/java/es/dm2egrupo07/accesoadatos/programa02/WeatherStatistics.java
@@ -1,7 +1,54 @@
 package es.dm2egrupo07.accesoadatos.programa02;
 
+import java.util.*;
+
 /**
  * Clase creada para calcular las estadísticas
  */
 public class WeatherStatistics {
+
+    public static List<CityStatistics> calculateStatistics(List<CityWeather> cities){
+        List<CityStatistics> statisticsList = new ArrayList<>();
+
+        for(CityWeather city : cities){
+            double avgTemperature = calculateAverage(city.getTemperatures());
+            double minTemperature = calculateMin(city.getTemperatures());
+            double maxTemperature = calculateMax(city.getTemperatures());
+            double avgHumidity = calculateAverage(city.getHumidities());
+            double avgPressure = calculateAverage(city.getPressures());
+
+            CityStatistics stats = new CityStatistics(city.getName(), avgTemperature, minTemperature, maxTemperature, avgHumidity, avgPressure);
+
+            statisticsList.add(stats);
+        }
+        return statisticsList;
+    }
+
+    private static double calculateMin(List<Double> values){
+        double min = Double.MAX_VALUE;
+        for(double value : values){
+            if(value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    private static double calculateMax(List<Double> values){
+        double max = Double.MIN_VALUE;
+        for (double value : values){
+            if(value > max){
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    private static double calculateAverage(List<Double> values){
+        double sum = 0;
+        for(double value : values){
+            sum += value;
+        }
+        return values.isEmpty() ? 0 : sum / values.size();
+    }
 }
